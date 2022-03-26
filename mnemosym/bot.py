a7621b5e37772eacb3e0ad86a8b264c40c089bde
Alex Sipiere <alex.sipiere@protonmail.ch>
2022-03-26 19:37:49 +0000

    new table methods and poc

--- mnemosym/bot.py
+++ mnemosym/bot.py
@@ -3,15 +3,23 @@ mnemosym.bot
 ~~~~~~~~~~~~
 Discord event handling
 """
+import json
 import os
+from pathlib import Path
+from random import choice
 
 from discord.ext import commands
 from dotenv import load_dotenv
 
 from mnemosym.chargen import generate_character
+from mnemosym.table_manger import TableManager
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+DATA_DIR = Path(os.getenv("DATA_DIR"))
+
+print("Initalizing Table Manager")
+tm = TableManager(DATA_DIR)
 
 bot = commands.Bot(command_prefix="!")
 
@@ -26,6 +34,13 @@ async def on_ready():
 async def build_random_character(ctx):
     """builds a random character"""
     await ctx.send(generate_character())
+
+
+@bot.command(name="ancestry", help="Generate a random Ancestry")
+async def build_random_ancestry(ctx):
+    """builds a random ancestry"""
+    target_ancestry = choice(list(tm.tables["ancestries"].keys()))
+    await ctx.send(json.dumps((tm.tables["ancestries"][target_ancestry].roll_table())))
 
 
 bot.run(TOKEN)
